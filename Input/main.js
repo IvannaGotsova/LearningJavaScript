7c860b416dfd5ad70f87ef58c5a43fef0721b452
IvannaGotsova <ivannagotsova@outlook.com>
2024-06-05 15:02:12 +0300

    Input

--- Input/main.js
+++ Input/main.js
@@ -8,3 +8,35 @@ function Information() {
     
     document.getElementById("information").innerText = information
 }
+
+function CheckBoxRadio() {
+    let yesCheckbox = document.getElementById("yes-checkbox")
+    let noCheckbox = document.getElementById("no-checkbox")
+    let oneRadio = document.getElementById("one-radio")
+    let twoRadio = document.getElementById("two-radio")
+    let threeRadio = document.getElementById("three-radio")
+
+    if (yesCheckbox.checked && noCheckbox.checked) {
+
+        document.getElementById("result").innerText = "Choose only one option!"
+
+    } else if (yesCheckbox.checked) {
+        if (oneRadio.checked && twoRadio.checked == false && threeRadio.checked == false) {
+
+            document.getElementById("result").innerText = "You choose option one!"
+
+        } else if (twoRadio.checked && oneRadio.checked == false && threeRadio.checked == false) {
+            
+            document.getElementById("result").innerText = "You choose option two!"
+
+        } else if (threeRadio.checked && twoRadio.checked == false && oneRadio.checked == false) {
+            
+            document.getElementById("result").innerText = "You choose option three!"
+        } else {
+
+            document.getElementById("result").innerText = "Choose only one option!"
+        }
+    } else {
+        document.getElementById("result").innerText = "You choose NO!"
+    }
+}
